a26327dfc05029ed9ade85fbadca17c925871666
VERA-OBIERO <vera.obiero@gmail.com>
2023-11-24 22:43:21 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    x = []
+
+    if length == 0:
+        pass
+    elif length == 1:
+        x.append(0)
+    elif length >= 2:
+        x = [0, 1]
+        for i in range(2, length):
+            x.append(x[i - 1] + x[i - 2])
+    
+    print(x) 
